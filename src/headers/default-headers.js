7ff79205af9e5aafe382d1358b074f673467d661
Kieran Simpson <kieran@redcrew.com.au>
2021-02-22 13:26:16 +1100

    Improve documentation.

--- src/headers/default-headers.js
+++ src/headers/default-headers.js
@@ -29,8 +29,19 @@ const { X_API_KEY, X_MERCHANT_ID, X_WALLET_ID } = require("./header-names");
 
 const { getPropOrError } = require("../helpers/props");
 
-// mapFunctions :: [ a -> b ] -> a -> [ b ]
-const mapFunctions = flip(pipe(applyTo, map));
+/*
+ * While `map` is very handy in that it will take a function and apply it to a value, what
+ * happens when we want to take an array of functions and apply them to a value?
+ *
+ * Because Array is a Functor (as it has a `map` method) we can take the array and apply it
+ * to a given value returning a new array with the results.
+ *
+ * `applyFunctor` can be used with any Functor without needing the Functor to be an Applicative
+ * or needing to lift the value into a Functor first; as is needed when applying an Applicative
+ * to a value.
+ */
+// applyFunctor :: Functor f => f (a -> b) -> a -> f b
+const applyFunctor = flip(pipe(applyTo, map));
 
 // getHeaderOrError :: String -> String -> Result Error Assign
 const getHeaderOrError = curry((prop, headerName) =>
@@ -59,7 +70,7 @@ const apiAuthenticatorToRequestHeaderFactory =
 // constantOptsToHeaders :: Object -> Result Error RequestHeaderFactory
 const constantOptsToHeaders =
 	pipe(
-		mapFunctions([
+		applyFunctor([
 			pipe(getHeaderOrError("apiKey", X_API_KEY)),
 			pipe(getHeaderOrNothing("walletId", X_WALLET_ID)),
 			pipe(getHeaderOrNothing("merchantId", X_MERCHANT_ID))
@@ -89,7 +100,7 @@ const accessTokenToHeader =
 // defaultHeaders :: Object -> Result Error RequestHeadersFactory
 const defaultHeaders =
 	pipe(
-		mapFunctions([
+		applyFunctor([
 			constantOptsToHeaders,
 			accessTokenToHeader
 		]),
