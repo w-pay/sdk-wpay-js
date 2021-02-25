4d6953f2a6584b1dd1ffd72723de347413d0a456
Kieran Simpson <kierans777@gmail.com>
2021-02-25 15:33:34 +1100

    Add tests for factory.

--- src/wpay-factory.js
+++ src/wpay-factory.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const curry = require("crocks/core/curry");
 const identity = require("crocks/combinators/identity");
 const pipeK = require("crocks/helpers/pipeK");
 
@@ -8,20 +9,20 @@ const { addHeaders, jsonMarshaller, resolveUrl } = require("@api-sdk-creator/htt
 const { defaultHeaders } = require("./headers/default-headers");
 const { resultHandler } = require("./api/result-handler");
 
-const createApiClient = (httpClient, options) => {
+// createApiClient :: HttpClientFactory -> Object -> SdkApiClient
+const createApiClient = curry((httpClient, options) => {
 	const headers = defaultHeaders(options).either((err) => {
 		throw err;
 	}, identity);
 
 	return pipeK(
-		// TODO: Test for this
 		resolveUrl(options.baseUrl),
 		addHeaders(headers),
 		jsonMarshaller(),
 		httpClient(),
 		resultHandler()
 	);
-};
+});
 
 module.exports = {
 	createApiClient
