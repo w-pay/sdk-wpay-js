d10caa9b17199d64eec8378d91fa316478b43377
Kieran Simpson <kieran@redcrew.com.au>
2021-09-04 17:31:36 +1000

    Cleaned up code.

--- src/helpers/props.js
+++ src/helpers/props.js
@@ -1,13 +1,9 @@
 "use strict";
 
-const curry = require("crocks/core/curry");
-const getProp = require("crocks/Maybe/getProp");
-const maybeToResult = require("crocks/Result/maybeToResult");
+const { getProp } = require("@epistemology-factory/crocks-ext/Result");
 
 // getPropOrError :: String -> Object -> Result Error a
-const getPropOrError = curry((prop, obj) =>
-	maybeToResult(new Error(`'${prop}' is mandatory and missing`), getProp(prop, obj))
-);
+const getPropOrError = getProp((prop) => new Error(`'${prop}' is mandatory and missing`));
 
 // toDate :: String -> Date
 const toDate = (s) => new Date(s);
