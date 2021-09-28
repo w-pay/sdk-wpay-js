e995bd8aa48e4ec3a6c69cc1ebbc60fd5e20fd29
Kieran Simpson <kieran@redcrew.com.au>
2021-09-28 17:19:53 +1000

    Fix to fetch tags.

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -3,6 +3,10 @@
 version=$(npm -s run env echo '$npm_package_version')
 version="v${version}"
 
+# When GH Actions checks out the repo it doesn't pull tags
+echo "Fetching tags"
+git fetch --tags
+
 if git show-ref --tags $version --quiet; then
   echo "Tag exists"
 else
