34f19de13609667dfc7337e59b46b31a26295d8f
Kieran Simpson <kieran@redcrew.com.au>
2021-09-28 15:51:15 +1000

    Only publish on main.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,8 +5,7 @@ version=$(npm -s run env echo '$npm_package_version')
 node $PWD/scripts/check-version-published.js $version
 
 if [ $? -eq 0 ] ; then
-  echo "Pretend publish"
-  #npm publish --access=public
+  npm publish --access=public
 else
   echo "No changes to publish"
 fi
