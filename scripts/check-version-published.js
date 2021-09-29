86e8df33c5183a0b5f65316209053b73c243ca9e
Kieran Simpson <kieran@redcrew.com.au>
2021-09-29 11:51:12 +1000

    Improved documentation.

--- scripts/check-version-published.js
+++ scripts/check-version-published.js
@@ -3,9 +3,18 @@
 const path = require("path");
 const { spawnSync } = require("child_process");
 
+/*
+ * Searches NPM for the @wpay/sdk module at a given version
+ *
+ * Exit codes
+ *  - 0 - Module not found at version
+ *  - 1 - Module found at version
+ *  - 2 - No version supplied
+ */
+
 if (process.argv.length < 3) {
 	console.error(`Usage: ${path.relative(process.cwd(), process.argv[1])} <version>`);
-	process.exit(1);
+	process.exit(2);
 }
 
 const packageData = fetchPackageData();
@@ -13,9 +22,8 @@ const version = process.argv[2];
 
 console.log(`Searching for ${version}`);
 const found = packageData.versions.find((v) => v === version);
-const result = checkResult(found, version);
 
-process.exit(result);
+process.exit(checkResult(found, version));
 
 function fetchPackageData() {
 	const data = spawnSync("npm", [ "view", "@wpay/sdk", "--json" ], { encoding: "utf8" }).stdout;
