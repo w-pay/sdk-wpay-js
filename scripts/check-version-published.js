80e001f46ccd7f259d875b08647907670205ef72
Kieran Simpson <kieran@redcrew.com.au>
2021-10-21 11:56:55 +1100

    Updated publishing to pass pkg name.
    Makes scripts more reusable.

--- scripts/check-version-published.js
+++ scripts/check-version-published.js
@@ -12,33 +12,34 @@ const { spawnSync } = require("child_process");
  *  - 2 - No version supplied
  */
 
-if (process.argv.length < 3) {
-	console.error(`Usage: ${path.relative(process.cwd(), process.argv[1])} <version>`);
+if (process.argv.length < 4) {
+	console.error(`Usage: ${path.relative(process.cwd(), process.argv[1])} <package> <version>`);
 	process.exit(2);
 }
 
-const packageData = fetchPackageData();
-const version = process.argv[2];
+const pkg = process.argv[2];
+const version = process.argv[3];
+const packageData = fetchPackageData(pkg);
 
-console.log(`Searching for ${version}`);
+console.log(`Searching for ${pkg}@${version}`);
 const found = packageData.versions.find((v) => v === version);
 
-process.exit(checkResult(found, version));
+process.exit(checkResult(found, pkg, version));
 
-function fetchPackageData() {
-	const data = spawnSync("npm", [ "view", "@wpay/sdk", "--json" ], { encoding: "utf8" }).stdout;
+function fetchPackageData(pkg) {
+	const data = spawnSync("npm", [ "view", pkg, "--json" ], { encoding: "utf8" }).stdout;
 
 	return JSON.parse(data);
 }
 
-function checkResult(result, version) {
+function checkResult(result, pkg, version) {
 	if (result !== undefined) {
-		console.log(`Found ${version}`);
+		console.log(`Found ${pkg}@${version}`);
 
 		return 1;
 	}
 	else {
-		console.log(`Didn't find ${version}`);
+		console.log(`Didn't find ${pkg}@${version}`);
 
 		return  0;
 	}
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
+name=$(npm -s run env echo '$npm_package_name')
 version=$(npm -s run env echo '$npm_package_version')
 
-node $PWD/scripts/check-version-published.js $version
+node $PWD/scripts/check-version-published.js $name $version
 
 if [ $? -eq 0 ] ; then
   npm publish --access=public
