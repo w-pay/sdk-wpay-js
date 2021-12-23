ef8c18a9c26488ab161b0f751154fbd71ac1b072
Kieran Simpson <kieran@redcrew.com.au>
2021-12-23 22:09:42 +1100

    Upgrade dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
+const webpack = require("webpack")
+
 module.exports = {
 	entry: {
 		all: path.join(__dirname, "/src/index.js"),
@@ -14,6 +16,17 @@ module.exports = {
 		library: "WPay",
 		libraryTarget: "umd"
 	},
+	plugins: [
+		/*
+		 * This is due to a bug in the npm "util" package that looks for the `process`
+		 * object in the browser and can't find it.
+		 *
+		 * See https://github.com/browserify/node-util/issues/43
+		 */
+		new webpack.DefinePlugin({
+			"process.env.NODE_DEBUG": JSON.stringify(process.env.NODE_DEBUG)
+		})
+	],
 	resolve: {
 		extensions: [ ".js" ],
 
