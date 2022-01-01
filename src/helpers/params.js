adfd3eb7ea96e5817dc29352ab7be61f8942549a
Kieran Simpson <kieran@redcrew.com.au>
2022-01-01 21:59:39 +1100

    Clean up to remove unnecessary named arg.

--- src/helpers/params.js
+++ src/helpers/params.js
@@ -1,24 +1,23 @@
 "use strict";
 
 const Assign = require("crocks/Assign");
-const curry = require("crocks/core/curry");
+
+const compose = require("crocks/helpers/compose");
+const curry = require("crocks/helpers/curry");
 const isDefined = require("crocks/predicates/isDefined");
 const map = require("crocks/pointfree/map");
 const mreduce = require("crocks/helpers/mreduce");
 const objOf = require("crocks/helpers/objOf");
 const option = require("crocks/pointfree/option");
-const pipe = require("crocks/helpers/pipe");
 const safe = require("crocks/Maybe/safe");
 
 // optionalParam = String -> a -> Object
-const optionalParam = curry((name, prop) =>
-	pipe(safe(isDefined), map(objOf(name)), option({}))(prop)
-);
+const optionalParam = (name) => compose(option({}), map(objOf(name)), safe(isDefined));
 
 // params = [ Objects ] -> Object
 const params = mreduce(Assign);
 
 module.exports = {
-	optionalParam,
+	optionalParam: curry(optionalParam),
 	params
 };
--- src/transformers/data.js
+++ src/transformers/data.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const Async = require("crocks/Async");
+
+const compose = require("crocks/helpers/compose");
 const curry = require("crocks/core/curry");
 const either = require("crocks/pointfree/either");
 const identity = require("crocks/combinators/identity");
@@ -12,14 +14,13 @@ const tryCatch = require("crocks/Result/tryCatch");
 const { getPropOrError } = require("../helpers/props");
 
 // transform :: (Object -> Object) -> Object -> Async Error Object
-const transform = curry((fn, obj) => pipe(tryCatch(fn), resultToAsync)(obj));
+const transform = (fn) => compose(resultToAsync, tryCatch(fn));
 
 // fromData :: (Object -> Object) -> Object -> Async Error Object
-const fromData = curry((fn, obj) =>
-	pipe(getPropOrError("data"), map(transform(fn)), either(Async.Rejected, identity))(obj)
-);
+const fromData = (fn) =>
+	pipe(getPropOrError("data"), map(transform(fn)), either(Async.Rejected, identity));
 
 module.exports = {
-	transform,
-	fromData: fromData
+	transform: curry(transform),
+	fromData: curry(fromData)
 };
