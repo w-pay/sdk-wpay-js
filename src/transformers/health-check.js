7649d3e2454246b964b89e5e3954badb0cbfcea6
Kieran Simpson <kieran@redcrew.com.au>
2022-02-21 11:52:21 +1100

    Fix test.
    Refactor to match other transformers in that if a prop is missing an error will occur.

--- src/transformers/health-check.js
+++ src/transformers/health-check.js
@@ -1,14 +1,13 @@
 "use strict";
 
-const compose = require("crocks/helpers/compose");
-const map = require("crocks/pointfree/map");
+const getPropOr = require("crocks/helpers/getPropOr");
 const objOf = require("crocks/helpers/objOf");
 const pipe = require("crocks/helpers/pipe");
 
-const { getPropOrError } = require("../helpers/props");
 const { toUpperCase } = require("../helpers/props");
 
 exports.fromHealthCheckDTO = pipe(
-	getPropOrError("healthCheck"),
-	map(compose(objOf("result"), toUpperCase))
+	getPropOr(undefined, "healthCheck"),
+	toUpperCase,
+	objOf("result")
 );
