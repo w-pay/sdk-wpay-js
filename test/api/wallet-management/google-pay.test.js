270e03ffa704b58afdfa74dc60f339b08eca97a0
Kieran Simpson <kieran@redcrew.com.au>
2022-04-04 14:00:09 +1000

    Fixed test to return correct response body.

--- test/api/wallet-management/google-pay.test.js
+++ test/api/wallet-management/google-pay.test.js
@@ -5,12 +5,11 @@ const { HttpRequestMethod } = require("@api-sdk-creator/http-api-client");
 const { StubApiClient } = require("../../stub-api-client");
 const { requiredParameterError } = require("../../matchers/required-parameters");
 const { v4: uuid } = require("uuid");
+
 const {
+	TokenizeGooglePayRequest,
 	TokenizeGooglePayResponseDTO
-} = require("../../data/wallet-management/TokenizeGooglePayResponse");
-const {
-	TokenizeGooglePayRequestDTO
-} = require("../../data/wallet-management/TokenizeGooglePayRequest");
+} = require("../../data/wallet-management/google-pay");
 const apiFactory = require("../../../src/api/wallet-management/google-pay");
 
 describe("GooglePay", function () {
@@ -20,41 +19,40 @@ describe("GooglePay", function () {
 	beforeEach(function () {
 		apiClient = new StubApiClient();
 		api = apiFactory(apiClient.client());
-		apiClient.response = {
-			data: TokenizeGooglePayResponseDTO(),
-			meta: {}
-		};
+		apiClient.response = TokenizeGooglePayResponseDTO();
 	});
 
 	describe("tokenize", function () {
 		it("should set request params", async function () {
-			await api.tokenize(TokenizeGooglePayRequestDTO());
+			await api.tokenize(TokenizeGooglePayRequest());
 			const request = apiClient.request;
 
 			assertThat(request.method, is(HttpRequestMethod.POST));
 			assertThat(request.url, is("/googlepay/tokenize"));
-			assertThat(request.body, is(TokenizeGooglePayRequestDTO()));
+			assertThat(request.body, is(TokenizeGooglePayRequest()));
 		});
 
-		it("should return TokenizeGooglePayResponseDTO", async function () {
-			const result = await api.tokenize(TokenizeGooglePayRequestDTO());
-			assertThat(result.data, is(TokenizeGooglePayResponseDTO()));
+		it("should tokenise", async function () {
+			const result = await api.tokenize(TokenizeGooglePayRequest());
+
+			assertThat(result, is(TokenizeGooglePayResponseDTO()));
 		});
 	});
 
 	describe("guestTokenize", function () {
 		it("should set request params", async function () {
-			await api.guestTokenize(TokenizeGooglePayRequestDTO());
+			await api.guestTokenize(TokenizeGooglePayRequest());
 			const request = apiClient.request;
 
 			assertThat(request.method, is(HttpRequestMethod.POST));
 			assertThat(request.url, is("/guest/googlepay/tokenize"));
-			assertThat(request.body, is(TokenizeGooglePayRequestDTO()));
+			assertThat(request.body, is(TokenizeGooglePayRequest()));
 		});
 
-		it("should return TokenizeGooglePayResponseDTO", async function () {
-			const result = await api.guestTokenize(TokenizeGooglePayRequestDTO());
-			assertThat(result.data, is(TokenizeGooglePayResponseDTO()));
+		it("should tokenize", async function () {
+			const result = await api.guestTokenize(TokenizeGooglePayRequest());
+
+			assertThat(result, is(TokenizeGooglePayResponseDTO()));
 		});
 	});
 
@@ -65,18 +63,19 @@ describe("GooglePay", function () {
 
 		it("should set request params", async function () {
 			const paymentToken = uuid();
-			await api.update(paymentToken, TokenizeGooglePayRequestDTO());
+			await api.update(paymentToken, TokenizeGooglePayRequest());
 
 			const request = apiClient.request;
 			assertThat(request.method, is(HttpRequestMethod.POST));
 			assertThat(request.url, is("/googlepay/tokenize/:paymentToken"));
 			assertThat(request.pathParams, is({ paymentToken }));
-			assertThat(request.body, is(TokenizeGooglePayRequestDTO()));
+			assertThat(request.body, is(TokenizeGooglePayRequest()));
 		});
 
-		it("should return TokenizeGooglePayResponseDTO", async function () {
-			const result = await api.update(uuid(), TokenizeGooglePayRequestDTO());
-			assertThat(result.data, is(TokenizeGooglePayResponseDTO()));
+		it("should update", async function () {
+			const result = await api.update(uuid(), TokenizeGooglePayRequest());
+
+			assertThat(result, is(TokenizeGooglePayResponseDTO()));
 		});
 	});
 });
